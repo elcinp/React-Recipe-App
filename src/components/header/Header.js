dca43d7f733fc02d41eadbd7b1a0d64e8592af5e
elcinp <elcinpars1@gmail.com>
2021-09-08 22:29:18 +0300

    Form.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./Form";
 import { HeaderContainer, MainHeader } from "./HeaderStyle";
+
 const Header = ({ setQuery, query, getData, mealTypes, setMeal, meal }) => {
   return (
     <HeaderContainer>
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,15 +2,17 @@ import { BrowserRouter,Switch,Route } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
+import Header from "../components/header/Header";
 
 function AppRouter() {
     return(
         <BrowserRouter>
-        <Switch>
-            <Navbar/>
-            <Route path="/" exact component={Home}/>
-            <Route path="/about" exact component={About}/>
-        </Switch>
+            <Switch>
+                <Navbar/>
+                <Route path="/" exact component={Home}/>
+                <Route path="/about" exact component={About}/>
+            </Switch>
+            <Header/>
         </BrowserRouter>
     )
 }
